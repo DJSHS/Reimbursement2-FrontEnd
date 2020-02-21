4bdc1f51968ce56c04afa8ed3fe6377667fa9158
Jia <l19960108@gmail.com>
2020-02-21 16:31:13 -0500

    get single reimbursement

--- Reimbursement2/src/app/components/reimbursement/reimbursement.component.ts
+++ Reimbursement2/src/app/components/reimbursement/reimbursement.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/services/auth-service/auth.service';
+import { ReimbursementService } from 'src/app/services/reimbursement-service/reimbursement.service';
+import { EmployeeService } from 'src/app/services/employee-service/employee.service';
+import { DepartmentService } from 'src/app/services/department-service/department.service';
+import { Reimbursement } from 'src/app/models/reimbursement';
+import { Employee } from 'src/app/models/employee';
+import { Department } from 'src/app/models/department';
 
 @Component({
   selector: 'app-reimbursement',
@@ -7,9 +15,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReimbursementComponent implements OnInit {
 
-  constructor() { }
+  currentReim: Reimbursement = new Reimbursement();
+  currentEmpl: Employee = new Employee();
+  currentDept: Department = new Department();
+
+  constructor(private router: Router, private route: ActivatedRoute, private authService: AuthService, private reimService: ReimbursementService, private emplService: EmployeeService, private deptService: DepartmentService) { }
 
   ngOnInit() {
+    // if (!this.authService.empl) {
+    //   this.router.navigate(['login']);
+    // } else {
+    //   this.route.params.subscribe(param => {
+    //     this.currentReim.reimId = param['id'];
+    //     this.getCurrentReim(this.currentReim.reimId);
+    //   })
+    // }
+    this.route.params.subscribe(param => {
+      this.currentReim.reimId = param['id'];
+      this.getCurrentReim(this.currentReim.reimId);
+    })
   }
 
+  getCurrentReim(reimId) {
+    this.reimService.getReimbursementById(reimId).subscribe(
+      data => {
+        // if (!data.reimId) {
+        //   this.router.navigate(['home']);
+        // } else if (data.submitById !== this.authService.empl.emplId && !this.authService.empl.isManager) {
+        //   this.router.navigate(['home']);
+        // } else {
+        //   this.currentReim = data;
+        //   this.getCurrentEmpl(this.currentReim.submitById);
+        // }
+        this.currentReim = data;
+        this.getCurrentEmpl(this.currentReim.submitById);
+      }, error => {
+        console.warn(error);
+      }
+    )
+  }
+
+  getCurrentEmpl(emplId) {
+    this.emplService.getEmployeeById(emplId).subscribe(
+      data => {
+        this.currentEmpl = data;
+        this.getCurrentDept(this.currentEmpl.deptId);
+      }, error => {
+        console.warn(error);
+      }
+    )
+  }
+
+  getCurrentDept(deptId) {
+    this.deptService.getDeptById(deptId).subscribe(
+      data => {
+        this.currentDept = data;
+      }, error => {
+        console.warn(error);
+      }
+    )
+  }
+
+  update() {
+
+  }
+
+  restore() {
+
+  }
+
+  delete() {
+
+  }
+
+  redirectToEmpl() {
+    if (this.authService.empl.isManager) {
+      this.router.navigate(['employees', this.currentReim.submitById]);
+    }
+  }
 }
